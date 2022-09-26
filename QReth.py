a7d1dd3b2148d33a9c76cf87d1692b07b543f91f
Pranesh Jahagirdar <44323581+pranesh-j@users.noreply.github.com>
2022-09-26 18:33:14 +0530

    Update QReth.py

--- QReth.py
+++ QReth.py
@@ -1,6 +1,5 @@
-#before getting started please run:  pip install qr[pil] in the cmd/shell, otherwise you wont see the QR code
-
 import qrcode
+from PIL import Image
 from web3 import Web3  # To interact with Ethereum blockchain using the web3.py library
 
 # With Infura, we have instant access to the Ethereum network via the HTTP and WebSocket protocols.
@@ -8,26 +7,39 @@ from web3 import Web3  # To interact with Ethereum blockchain using the web3.py 
 infura_url = ("https://mainnet.infura.io/v3/8471a36e96334a3c918afd7107692b11")
 w3 = Web3(Web3.HTTPProvider(infura_url))
 
+
+# Add Logo In the Center
+Logo_link = 'eth.png'
+logo = Image.open(Logo_link)
+basewidth = 100
+ 
+# adjust image size
+wpercent = (basewidth/float(logo.size[0]))
+hsize = int((float(logo.size[1])*float(wpercent)))
+logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
+qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+
 address = input("Enter the Eth wallet address: \n")
 
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-#Now To generate the QR code specific to Ethereum wallet 
-#Create a function to know whether the address provided is address an Ethereum address 
+#Now To generate QR code specific to Ethereum wallet address
 
 is_address_valid = w3.isAddress(address)
 
 if is_address_valid == True:
 
-    qr.add_data(address)
-    qr.make(fit=True)
-    img = qr.make_image(fill_color="#716b94", back_color="black")
-    img.save("QReth.png")
-    print("Grats your QReth created")
+  qr.add_data(address)
+  qr.make()
+  img = qr.make_image(fill_color="#716b94", back_color="black") 
+
+  position = ((img.size[0] - logo.size[0]) // 2,
+         (img.size[1] - logo.size[1]) // 2)
+  img.paste(logo, position)
+
+  img.save("QReth.png")
+
+
+  print("Grats your QReth is created")
 
 else:
-    print("Enter *Only* Ethereum wallet address")
+ print("Enter *Only* Ethereum wallet address! Ex: 0xF9e82768a7ED3f08b27EF11083B3BCF0A7E8A69e")
+
